c255c0225f234a76637ba760a7bdf96298ce41a9
Claire <claire0505@gmail.com>
2018-10-19 18:14:41 +0800

    1.FireBase的Email與密碼註冊(當使用者登入一組為加入過的帳密時會跳出來詢問。  2.將填入過的Email使用SharedPreferences儲存起來

--- app/src/main/java/com/claire/firebasecontacts/LoginActivity.java
+++ app/src/main/java/com/claire/firebasecontacts/LoginActivity.java
@@ -1,12 +1,18 @@
 package com.claire.firebasecontacts;
 
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -20,6 +26,13 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        //填入已儲存帳號
+        EditText email = findViewById(R.id.ed_email);
+        SharedPreferences setting =
+                getSharedPreferences("contacts", MODE_PRIVATE);
+        email.setText(setting.getString("PREF_EMAIL", ""));
+
+
         auth = FirebaseAuth.getInstance();
         authStateListener = new FirebaseAuth.AuthStateListener() {
             @Override
@@ -28,6 +41,7 @@ public class LoginActivity extends AppCompatActivity {
                 if (user!= null){
                     Log.d("onAuthStateChanged: ", "登入：" + user.getUid());
                     userUID = user.getUid();
+
                 }else {
                     Log.d("onAuthStateChanged: ", "已登出");
                 }
@@ -50,9 +64,54 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     public void login(View view) {
-        String email = ((EditText)findViewById(R.id.ed_email)).getText().toString();
-        String password = ((EditText)findViewById(R.id.ed_password)).getText().toString();
+        final String email = ((EditText)findViewById(R.id.ed_email)).getText().toString();
+        final String password = ((EditText)findViewById(R.id.ed_password)).getText().toString();
+
+        //儲存登入Email
+        SharedPreferences pref = getSharedPreferences("contacts", MODE_PRIVATE);
+        pref.edit()
+                .putString("PREF_EMAIL", email)
+                .apply();
+
         Log.d("AUTH", "login: " + email + "/" + password);
-        auth.signInWithEmailAndPassword(email, password);
+        auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (!task.isSuccessful()){
+                            Log.d("onComplete: ", "登入失敗");
+                            register(email, password);
+                        }
+                    }
+                });
+    }
+
+    private void register(final String email, final String password) {
+        new AlertDialog.Builder(LoginActivity.this)
+                .setTitle("登入問題")
+                .setMessage("無此帳號，是否要以此帳號與密碼註冊?")
+                .setPositiveButton("註冊", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        createUser(email, password);
+                    }
+                })
+                .setNeutralButton("取消", null)
+                .show();
+    }
+
+    private void createUser(String email, String password) {
+        auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        String message =
+                                task.isComplete()? "註冊成功" : "註冊失敗";
+                        new AlertDialog.Builder(LoginActivity.this)
+                                .setMessage(message)
+                                .setPositiveButton("OK", null)
+                                .show();
+                    }
+                });
     }
 }
