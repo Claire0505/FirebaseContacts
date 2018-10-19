558ca516aac1a30f2ed9fe1195480a7767a8504b
Claire <claire0505@gmail.com>
2018-10-19 22:02:01 +0800

    使用setValue與push儲存資料

--- app/src/main/java/com/claire/firebasecontacts/LoginActivity.java
+++ app/src/main/java/com/claire/firebasecontacts/LoginActivity.java
@@ -3,6 +3,7 @@ package com.claire.firebasecontacts;
 import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -15,6 +16,12 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class LoginActivity extends AppCompatActivity {
     FirebaseAuth auth;
@@ -41,6 +48,8 @@ public class LoginActivity extends AppCompatActivity {
                 if (user!= null){
                     Log.d("onAuthStateChanged: ", "登入：" + user.getUid());
                     userUID = user.getUid();
+                    //setUserData();
+                    //pushFriend("Jack");
 
                 }else {
                     Log.d("onAuthStateChanged: ", "已登出");
@@ -113,5 +122,47 @@ public class LoginActivity extends AppCompatActivity {
                                 .show();
                     }
                 });
+    }
+
+    //將資料存在FireBase上
+    private void setUserData(){
+        //將資料儲存在[users]記錄下，在取得資料庫物件後，再取得users的參照
+        FirebaseDatabase db = FirebaseDatabase.getInstance();
+        DatabaseReference usersRef = db.getReference("users");
+
+        //接著呼叫child方法代表要在users記錄下新增子記錄，再呼叫setValue方法，
+        //在users記錄下新增資料
+        usersRef.child(userUID).child("phone").setValue("0920123456");
+        usersRef.child(userUID).child("nickname").setValue("Hank");
+
+        //需要更新記錄時，可使用Map集合配合updateChildren方法
+        Map<String, Object> data = new HashMap<>();
+        data.put("nickname", "Hank123");
+        usersRef.child(userUID).updateChildren(data,
+                new DatabaseReference.CompletionListener() {
+                    @Override
+                    public void onComplete(@Nullable DatabaseError databaseError,
+                                           @NonNull DatabaseReference databaseReference) {
+                        if (databaseError != null){
+                            //正確完成
+                        } else {
+                            //發生錯誤
+                        }
+                    }
+                });
+    }
+
+    //使用push方法
+    private void pushFriend(String name){
+        FirebaseDatabase db = FirebaseDatabase.getInstance();
+        DatabaseReference usersRef = db.getReference("users");
+        DatabaseReference friendsRef =
+                usersRef.child(userUID).child("friends").push();
+        Map<String, Object> friend = new HashMap<>();
+        friend.put("name", name);
+        friend.put("phone","22334455");
+        friendsRef.setValue(friend);
+        String friendId = friendsRef.getKey();
+        Log.d("FRIEND",friendId + "");
     }
 }
